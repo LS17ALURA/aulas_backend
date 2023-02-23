7d91205cb8aca5ddf0d266823e69f9cdb821e9e8
Lavinia Sandi <10246769602@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-02-23 11:24:07 -0300

    aula while

--- aula4/repeticao-for.php
+++ aula4/repeticao-for.php
@@ -1,3 +1,16 @@
+<style>
+    div{
+        border: 1px solid #000;
+    }
+    .container{
+        display: grid;
+        grid-template-rows: auto auto ;
+        grid-template-columns: auto auto auto auto auto;
+    }
+    .tabuada{
+        text-align: justify;
+    }
+</style>
 <h2>Estrutura de repetição for</h2>
 <p>
     Utilizada para repetir uma sequência de instrução um predeterminado número de vezes.
@@ -9,10 +22,16 @@
         }
     </pre>
 </p>
+<div class="container">
 <?php
-    
-    for ($contador=0; $contador <= 10; $contador++) { 
-        echo nl2br("2 x $contador =" . $contador*2 . PHP_EOL);
-    }
-    // transformar na tabuada completa
+        //valor inicial; condição de parada; step -> passo
+    for ($contador=1; $contador <= 10; $contador++) { 
+        echo "<div class='tabuada'>"."<p>";
+        for ($contador2=0; $contador2 <= 10; $contador2++) { 
+            echo nl2br("&nbsp;&nbsp;&nbsp;&nbsp;$contador x $contador2 =" . $contador*$contador2 . PHP_EOL);
+            
+        }        
+        echo "</div>";
+     }
 ?>
+</div>
