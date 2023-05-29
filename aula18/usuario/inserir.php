9a93f658a475cc2e7a616431a8d61fd3c9299f78
Lavinia Sandi <10246769602@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-05-29 08:50:41 -0300

    session

--- aula18/usuario/inserir.php
+++ aula18/usuario/inserir.php
@@ -6,26 +6,26 @@ if(isset($_POST["nome"]) && isset($_POST["login"])
            && isset($_POST["senha"]))
 {
 
-//inclui o arquivo para salvar a foto do upload
-require_once "salvar_foto.php";
+        //inclui o arquivo para salvar a foto do upload
+        require_once "salvar_foto.php";
 
-$nome =  $_POST["nome"];
-$login = $_POST["login"];
-$senha = password_hash($_POST["senha"], PASSWORD_DEFAULT);
-$foto =  $nome_arquivo;
+        $nome =  $_POST["nome"];
+        $login = $_POST["login"];
+        $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT);
+        $foto =  $nome_arquivo;
 
-//String com o comando SQL para ser executado no DB
-$sql = "INSERT INTO `usuario`(`nome`, `login`, `senha`, `foto`) 
-        VALUES (?, ?, ?, ?);";
+        //String com o comando SQL para ser executado no DB
+        $sql = "INSERT INTO `usuario`(`nome`, `login`, `senha`, `foto`) 
+                VALUES (?, ?, ?, ?);";
 
-//Prepara o SQL para ser executado no banco de dados
-$comando = $conexao->prepare($sql);
+        //Prepara o SQL para ser executado no banco de dados
+        $comando = $conexao->prepare($sql);
 
-//adiciona os valores nos parâmetros
-$comando->bind_param("ssss", $nome, $login, $senha, $foto);
+        //adiciona os valores nos parâmetros
+        $comando->bind_param("ssss", $nome, $login, $senha, $foto);
 
-//executa o SQL - Comando no Banco de Dados
-$comando->execute();
+        //executa o SQL - Comando no Banco de Dados
+        $comando->execute();
 
 }
 //abre o arquivo form.php
