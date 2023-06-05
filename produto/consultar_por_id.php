c021816d7cabceda32661163d2bceae0dae567d4
Lavinia <lavinia.sandi@alunos.ifsuldeminas.edu.br>
2023-06-05 09:07:40 -0300

    produto tabela

--- produto/consultar_por_id.php
+++ produto/consultar_por_id.php
@@ -1,19 +1,15 @@
 <?php
-    require_once "../controla_sessao/controla.php";
-?>
-<?php
 
 require_once "../conexao.php";
 
 //verifica se foi enviado o param id pela URL
-if(isset($_GET['id']))
-{
+if(isset($_GET['id'])){
 
     //pega o valor do id que foi enviado pela URL
     $id = $_GET['id'];
 
     //String com o comando SQL para ser executado no DB
-    $sql = "DELETE FROM `produto` WHERE  `idproduto`= ? ; ";
+    $sql = "SELECT * FROM `produto` WHERE  `idproduto`= ? ; ";
 
     //Prepara o SQL para ser executado no banco de dados
     $comando = $conexao->prepare($sql);
@@ -24,7 +20,10 @@ if(isset($_GET['id']))
     //executa o SQL - Comando no Banco de Dados
     $comando->execute();
 
-}
+    //pegar o resultado da consulta
+    $resultado = $comando->get_result();
 
-//abre o arquivo form.php
-header("Location: index.php");
+    //pegar a primeira linha de resultado
+    $produto = $resultado->fetch_assoc();
+
+}
